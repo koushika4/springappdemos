aee9c28c192f7d7b1128991dbc5bcfc61ab7929b
koushikabalusu <koushika1204@sasi.ac.in>
2022-08-16 13:11:53 +0530

    changes

---
+++ s01springcoredi/src/main/java/com/samples/s01springcoredi/App.java
@@ -0,0 +1,31 @@
+package com.samples.s01springcoredi;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/s01springcoredi/SpringConfig.xml");
+       
+ //      Employee emp = (Employee) springContainer.getBean("emp"); 
+   //    System.out.println(emp);
+       
+//     Employee em1 = new Employee();
+//     em1.setId(10);
+//     em1.setName("x");
+
+//     Hospital hospital = (Hospital) springContainer.getBean("hospital");
+//     System.out.println(hospital);
+     
+     
+//     CarDealer dealer = (CarDealer) springContainer.getBean("cardealer");
+//     System.out.println(dealer);
+     
+     Customer customer = (Customer) springContainer.getBean("customer");
+     System.out.println(customer);
+
+       
+    }
+    
+}
